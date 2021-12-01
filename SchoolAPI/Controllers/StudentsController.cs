e387810a47e1b45bb66147c7db27ee971333e9dd
Edouma <edoumacodu@gmail.com>
2021-12-01 08:43:39 +0300

    Removed weatherforecast controller

--- SchoolAPI/Controllers/StudentsController.cs
+++ SchoolAPI/Controllers/StudentsController.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SchoolAPI.Data;
-using SchoolAPI.Models;
 using SchoolAPI.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SchoolAPI.Controllers
 {
